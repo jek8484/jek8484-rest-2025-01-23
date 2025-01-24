b2413359ad1334600f28a2494e04c45877f13e0e
jek8484 <ysms3611@gmail.com>
2025-01-24 17:19:55 +0900

    work

---
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -0,0 +1,131 @@
+package com.ll.rest.domain.post.post.controller;
+
+import com.ll.rest.domain.post.post.dto.PostDto;
+import com.ll.rest.domain.post.post.entity.Post;
+import com.ll.rest.domain.post.post.service.PostService;
+import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class ApiV1PostController {
+    private final PostService postService;
+
+    @GetMapping
+    public List<PostDto> getItems() {
+        return postService
+                .findAllByOrderByIdDesc()
+                .stream()
+                .map(PostDto::new)
+                .toList();
+    }
+
+
+    @GetMapping("/{id}")
+    public PostDto getItem(
+            @PathVariable long id
+    ) {
+        return postService.findById(id)
+                .map(PostDto::new)
+                .orElseThrow();
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<RsData<Void>> deleteItem(
+            @PathVariable long id
+    ) {
+        Post post = postService.findById(id).get();
+
+        postService.delete(post);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        new RsData<>(
+                                "200-1",
+                                "%d번 글이 삭제되었습니다.".formatted(id),
+                                null
+                        )
+                );
+    }
+
+
+    record PostModifyReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData<PostDto> modifyItem(
+            @PathVariable long id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(id).get();
+
+        postService.modify(post, reqBody.title, reqBody.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(id),
+                new PostDto(post)
+        );
+    }
+
+    record PostWriteReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content
+    ) {
+    }
+
+    record PostWriteResBody(
+            PostDto postDto,
+            long totalCount
+    ) {
+    }
+
+    @PostMapping
+    public ResponseEntity<RsData<PostWriteResBody>> writeItem(
+            @RequestBody @Valid PostWriteReqBody reqBody
+    ) {
+        Post post = postService.write(reqBody.title, reqBody.content);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        new RsData<>(
+                            "201-1",
+                            "%d번 글이 작성되었습니다.".formatted(post.getId()),
+                            new PostWriteResBody(
+                                    new PostDto(post),
+                                    postService.count()
+                            )
+                        )
+                );
+    }
+}
---
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -0,0 +1,30 @@
+package com.ll.rest.domain.post.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ll.rest.domain.post.post.entity.Post;
+import com.ll.rest.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostDto {
+    private long id;
+    @JsonProperty("createdDatetime")
+    private LocalDateTime createDate;
+    @JsonProperty("modifiedDatetime")
+    private LocalDateTime modifyDate;
+    private String title;
+    private String content;
+
+    public PostDto(Post post) {
+        this.id = post.getId();
+        this.createDate = post.getCreateDate();
+        this.modifyDate = post.getModifiyDate();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
+}
